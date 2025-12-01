1346b476acbb841399a3f85ed319fc8e8402e351
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-12-01 14:32:51 +0530

    update

---
+++ Day-1/loop.js
@@ -0,0 +1,17 @@
+//  for ( i=1 ; i<= 10 ; i++ ) {
+//     console.log(i)
+//  }
+
+// let n = 5 ;
+// let sum = 0 ;
+// for (let i = 1 ; i <= n ; i++){
+//     sum += i ;
+// }
+// console.log(sum);
+
+let str = "om";
+let reverse = "";
+for ( let i = str.length - 1 ; i >= 0 ; i--){
+    reverse += str[i];
+}
+console.log(reverse);

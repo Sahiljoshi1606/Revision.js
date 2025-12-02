7314cf6a1b1147f0619253efc7f19f187a7b012c
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-12-02 21:25:19 +0530

    update

--- Day-1/loop.js
+++ Day-1/loop.js
@@ -9,9 +9,40 @@
 // }
 // console.log(sum);
 
-let str = "om";
-let reverse = "";
-for ( let i = str.length - 1 ; i >= 0 ; i--){
-    reverse += str[i];
-}
-console.log(reverse);
+// let str = "om";
+// let reverse = "";
+// for ( let i = str.length - 1 ; i >= 0 ; i--){
+//     reverse += str[i];
+// }
+// console.log(reverse);
+
+
+// let str = "poiuytrewqasdfghj"
+// let reverse = ""
+// for (let i = str.length -1 ; i >=0 ; i--){
+//     reverse += str[i];
+// }
+// console.log(reverse);
+
+
+// let arr = [3, 1, 5, 7, 2];
+// let max = arr[0];
+// for (let i = 1 ; i < arr.length ; i++){
+//     if (arr[i] < max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max)
+
+// let arr = [1, 2, 3, 2, 1, 4];
+// let result = [];
+// for ( let i = 0 ; i < arr.length ; i++){
+//     if(!result.includes (arr[i]) ){
+//         result.push(arr[i])
+//     }
+// }
+// console.log(result)
+
+let age = 20
+const canvote = age > 21 ? "yes vote" : "cant vote"
+console.log(canvote)
